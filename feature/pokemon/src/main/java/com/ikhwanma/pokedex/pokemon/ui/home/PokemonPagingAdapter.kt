ccef94569cd094b77603f4a4475f0f152ab04851
ikhwanma <ikhwan749@gmail.com>
2024-06-25 16:02:13 +0700

    update paging adapter using base ui

--- feature/pokemon/src/main/java/com/ikhwanma/pokedex/pokemon/ui/home/PokemonPagingAdapter.kt
+++ feature/pokemon/src/main/java/com/ikhwanma/pokedex/pokemon/ui/home/PokemonPagingAdapter.kt
@@ -1,41 +1,29 @@
 package com.ikhwanma.pokedex.pokemon.ui.home
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.paging.PagingDataAdapter
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.ikhwanma.base_ui.BaseRecyclerViewPagingAdapter
 import com.ikhwanma.model.Pokemon
 import com.ikhwanma.pokedex.feature.pokemon.databinding.ItemPokemonGridBinding
 
-class PokemonPagingAdapter: PagingDataAdapter<Pokemon, PokemonPagingAdapter.ViewHolder>(
-    object : DiffUtil.ItemCallback<Pokemon>() {
-        override fun areItemsTheSame(oldItem: Pokemon, newItem: Pokemon): Boolean =
-            oldItem.pokemonName == newItem.pokemonName
+class PokemonPagingAdapter: BaseRecyclerViewPagingAdapter<Pokemon, ItemPokemonGridBinding>() {
 
-        override fun areContentsTheSame(oldItem: Pokemon, newItem: Pokemon): Boolean =
-            oldItem.pokemonName == newItem.pokemonName
-    }
-) {
+    private lateinit var context: Context
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val binding =
-            ItemPokemonGridBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return ViewHolder(binding)
+    override fun getViewBinding(parent: ViewGroup): ItemPokemonGridBinding {
+        context = parent.context
+        return ItemPokemonGridBinding.inflate(LayoutInflater.from(context), parent, false)
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        getItem(position)?.let { holder.bind(it) }
-    }
-
-    inner class ViewHolder(private val binding: ItemPokemonGridBinding) :
-        RecyclerView.ViewHolder(binding.root) {
-        fun bind(data: Pokemon) {
-            with(binding) {
-                Glide.with(itemView).load(data.imageUrl).into(ivPokemon)
-                tvPokemonName.text = data.pokemonName
-            }
+    override fun bindItem(binding: ItemPokemonGridBinding, data: Pokemon, position: Int) {
+        with(binding) {
+            Glide.with(context).load(data.imageUrl).into(ivPokemon)
+            tvPokemonName.text = data.pokemonName
         }
     }
 }
